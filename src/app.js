a6cbcb35f737eece5dd657a23dd527c05f77d38c
Berkay <berkay.ergun16@gmail.com>
2023-02-14 14:57:28 +0300

    Ejs template added.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+
 const app = express();
+
 dotenv.config();
+
+const __dirname = path.resolve() + '/src';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 // static files
-app.use(express.static('src/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+
+app.listen(process.env.PORT || 5000 , () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
