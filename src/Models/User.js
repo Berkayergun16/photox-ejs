6c822415fd69f31a8329ec9b4d198fcd2d02969f
Berkay <berkay.ergun16@gmail.com>
2023-02-15 17:19:17 +0300

    hash password

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -25,6 +25,14 @@ const UserSchema = new mongoose.Schema({
     timestamps: true,
 }
 );
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', UserSchema);
 
