a0b41f27121cf41cd53cf8ad0860172f27cb5a30
Berkay <berkay.ergun16@gmail.com>
2023-03-04 21:44:10 +0300

    send mail added

---
+++ src/helpers/mail/sendMail.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+import htmlMailTemplate from "./htmlMailTemplate.js";
+
+const mailTo = "berkay.ergun16@gmail.com"
+
+const sendMail = async (req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+        user: process.env.NODE_MAIL,
+        pass: process.env.NODE_MAIL_PASS, 
+        },
+    });
+    await transporter.sendMail({
+        to: mailTo, 
+        subject: `MAIL FROM ${req.body.email}`, 
+        text: "Hello world?", 
+        html: htmlMailTemplate(req),
+      });
+
+}
+
+export default sendMail;
--- src/routers/pageRoute.js
+++ src/routers/pageRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getIndexPage ,getAboutPage,getRegisterPage,getLoginPage} from "../controllers/pageController.js";
+import { getIndexPage ,getAboutPage,getRegisterPage,getLoginPage,getContactPage,sendMailInContact} from "../controllers/pageController.js";
 import {authenticateToken} from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.get('/',getIndexPage);
 router.get('/about', getAboutPage);
 router.get('/register', getRegisterPage);
 router.get('/login', getLoginPage);
+router.get('/contact', getContactPage);
+
+router.post('/contact', sendMailInContact);
 
 export default router;
