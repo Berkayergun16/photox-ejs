ec65ae0c1b175629c220e4949e9bac9a93498e0e
Berkayergun16 <berkay.ergun16@gmail.com>
2023-03-07 14:50:21 +0300

    Edit for nodejs structure

--- src/routers/photoRoute.js
+++ src/routers/photoRoute.js
@@ -1,17 +1,26 @@
-
+// Modules
 import express from "express";
-import { createPhoto ,getAllPhotos, getPhotoDetail,updatePhoto,deletePhoto} from "../controllers/photoController.js";
-import { authenticateToken } from "../middlewares/authMiddleware.js";
 
+// Controllers
+import {
+  createPhoto,
+  getAllPhotos,
+  getPhotoDetail,
+  updatePhoto,
+  deletePhoto,
+} from "../controllers/photoController.js";
+
+// Middlewares
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/', createPhoto);
-router.get('/', getAllPhotos)
-router.get('/:id', getPhotoDetail);
+// Routes
+router.post("/", createPhoto);
+router.get("/", getAllPhotos);
+router.get("/:id", getPhotoDetail);
 
-router.put('/:id/update',authenticateToken , updatePhoto);
-router.delete('/:id/delete',authenticateToken, deletePhoto);
-
+router.put("/:id/update", authenticateToken, updatePhoto);
+router.delete("/:id/delete", authenticateToken, deletePhoto);
 
 export default router;
--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -1,17 +1,31 @@
+// Modules
 import express from "express";
-import { addUser ,loginUser,getDashboardPage,logoutUser,getAllUsers, getUserDetails, followUser, unfollowUser} from "../controllers/userController.js";
-import {authenticateToken} from '../middlewares/authMiddleware.js';
+
+// Controllers
+import {
+  addUser,
+  loginUser,
+  getDashboardPage,
+  logoutUser,
+  getAllUsers,
+  getUserDetails,
+  followUser,
+  unfollowUser,
+} from "../controllers/userController.js";
+
+// Middlewares
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
-router.get('/',authenticateToken ,getAllUsers);
-router.get('/detail/:id',authenticateToken, getUserDetails);
+router.get("/", authenticateToken, getAllUsers);
+router.get("/detail/:id", authenticateToken, getUserDetails);
 
-router.post('/register', addUser);
-router.post('/login', loginUser);
-router.get('/dashboard',authenticateToken, getDashboardPage);
-router.get('/logout',authenticateToken, logoutUser);
+router.post("/register", addUser);
+router.post("/login", loginUser);
+router.get("/dashboard", authenticateToken, getDashboardPage);
+router.get("/logout", authenticateToken, logoutUser);
 
-router.put('/:id/follow',authenticateToken, followUser);
-router.put('/:id/unfollow',authenticateToken, unfollowUser);
+router.put("/:id/follow", authenticateToken, followUser);
+router.put("/:id/unfollow", authenticateToken, unfollowUser);
 
 export default router;
